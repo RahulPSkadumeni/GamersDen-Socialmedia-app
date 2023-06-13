97075f3cc71612084931749898aaf025d0c9b55e
Rahul P S <rahulps995@gmail.com>
2023-06-13 11:42:16 +0530

    ui update

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 const Axios = axios.create({
-  // baseURL: "https://projecthostvercel.vercel.app/api",
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://projecthostvercel.vercel.app/api",
 });
 
 export default Axios;
-// baseURL: "http://localhost:3001/api",
-//baseURL: "https://gamersden.tech/api",
--- client/src/utils/baseurl.js
+++ client/src/utils/baseurl.js
@@ -1,3 +1,3 @@
 const BASE_URL = "https://projecthostvercel.vercel.app/api";
-//const BASE_URL = "http://localhost:3001/api";
+
 export default BASE_URL;
